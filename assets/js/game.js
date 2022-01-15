7cc5f3cdd9abba0f232a116ed39276f4a4505f1c
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-01-15 13:29:22 -0500

    added option to skip or fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,21 +11,30 @@ var enemyAttack = 12;
 
 var fight = function() {
   window.alert("Welcome to Robot Gladiators!");
-  enemyHealth = enemyHealth - playerAttack;
-  console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
-  if (enemyHealth <= 0) {
-    window.alert(enemyName + " has died!");
-  } 
-  else {
-    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+  if(promptFight.toUpperCase() === "FIGHT") {
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    if (enemyHealth <= 0) {
+      window.alert(enemyName + " has died!");
+    } 
+    else {
+      window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+    playerHealth = playerHealth - enemyAttack;
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+    if (playerHealth <= 0) {
+      window.alert(playerName + " has died!");
+    } 
+    else {
+      window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
   }
-  playerHealth = playerHealth - enemyAttack;
-  console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
-  if (playerHealth <= 0) {
-    window.alert(playerName + " has died!");
-  } 
+  else if(promptFight.toUpperCase() === "SKIP") {
+    window.alert(playerName + " has chosen to skip the fight!");
+  }
   else {
-    window.alert(playerName + " still has " + playerHealth + " health left.");
+    window.alert("you need to choose a valid option. Try again!");
   }
 }
 fight();
